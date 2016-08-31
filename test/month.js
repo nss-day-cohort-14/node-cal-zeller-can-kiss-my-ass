823c0689dd92a9e13470af8026b26330730f080b
Dominic Serrano <dmsrojo@gmail.com>
2016-08-31 11:34:02 -0500

    Set up testing for month and getWeeks module

--- test/month.js
+++ test/month.js
@@ -1,12 +1,56 @@
 'use strict';
 
-const { isFunction } = require('chai').assert;
-const month = require('../lib/month');
+const { isFunction, strictEqual, oneOf } = require('chai').assert;
+const { getWeeks, makeMonth } = require('../lib/month');
+const { zeller } = require('../lib/zeller');
+const { cal } = require('../lib/cal');
 
 
 describe('month module', () => {
 
-  it('should be a function', () => {
+  describe('getWeeks', () => {
+    it('should be a function', () => {
+        isFunction(getWeeks);
+    });
+    it('should return 4 weeks', () => {
+        let starts = 0, days = 28;
+        strictEqual(getWeeks(starts, days), 4);
+    });
+    it('should return 5 weeks', () => {
+        let starts = 1, days = 31;
+        strictEqual(getWeeks(starts, days), 5);
+    });
+    it('should return 6 weeks', () => {
+        let starts = 6, days = 30;
+        strictEqual(getWeeks(starts, days), 6);
+    });
+  });
+
+
+  describe('makeMonth()', () => {
+
+    it('should be a function', () => {
+      isFunction(makeMonth);
+    });
+
+    describe('June 2016', () => {
+      let month = 6,
+      year = 2016,
+      start = zeller(month, year);
+
+      it('shoud return Start day of week', () => {
+        strictEqual(start, 3);
+      });
+
+      it('should return number of weeks', () => {
+        strictEqual(getWeeks(start, cal[month - 1].days), 5);
+      });
+
+      it('should return number of days in month', () => {
+        strictEqual(cal[month - 1].days, 30);
+      });
+
+    });//End 'June 2016'
 
   });
 
