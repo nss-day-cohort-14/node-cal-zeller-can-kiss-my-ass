e322cb9689d456ef5e25573193c487674113526e
Dominic Serrano <dmsrojo@gmail.com>
2016-08-31 10:14:44 -0500

    Ran integration testing for cli

---
+++ test/month.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { isFunction } = require('chai').assert;
+const month = require('../lib/month');
+
+
+describe('month module', () => {
+
+  it('should be a function', () => {
+
+  });
+
+});
